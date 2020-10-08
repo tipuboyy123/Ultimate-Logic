8b6e8b3ad98def9f80a6b7c0d26065821102f04e
Freyam Mehta <62539811+freyam@users.noreply.github.com>
2020-10-08 21:26:16 +0400

    Update octal.c

--- octal/octal.c
+++ octal/octal.c
@@ -2,7 +2,7 @@
 
 int main() {
     int num;
-    printf("Decimal Number (0 and 32767): ");
+    printf("Decimal Number (0 ... 32767): ");
     scanf("%d", &num);	
 
     printf("%d in Octal is ~%o~", num, num);
